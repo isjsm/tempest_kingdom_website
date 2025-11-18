d17a6fafc8f4a3114027254bdc826ed27190ae54
isjsm <84210598+isjsm@users.noreply.github.com>
2025-11-18 17:58:11 +0300

    Create branches-data.js

---
+++ js/data/branches-data.js
@@ -0,0 +1,13 @@
+// js/data/branches-data.js
+
+const branchesData = [
+    {
+        id: 'main_branch',
+        name: 'الصحيفة الرسمية للمملكة',
+        description: 'هنا يتم الاعلان عن الاعلانات الهامة مثل انضمام نقابات او خروجها او افتتاحها و يقام مسابقات على ارقام كل فترة',
+        icon: 'fa-solid fa-crown',
+        joinLink: 'https://chat.whatsapp.com/YOUR_MAIN_GROUP_LINK', // <-- ضع رابط مجموعتك هنا
+        isPrimary: true // لتمييز الفرع الرئيسي
+    }
+    // يمكنك إضافة المزيد من الفروع هنا
+];
